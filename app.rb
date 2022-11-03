2016da542bd36c009bd50acd2258224aa2c51ecc
Tom Fyfe <tomfyfe@MASTUDENT-Tom-Fyfe-W7JGKXKG9H.local>
2022-11-03 13:53:33 +0000

    forms

--- app.rb
+++ app.rb
@@ -1,41 +1,58 @@
 require 'sinatra/base'
+require "sinatra/reloader"
+
 
 class Application < Sinatra::Base
- #GET /
- # Root path (home page/index page)
-  get '/' do 
-   return 'Hello'
-  end 
-
-  get '/posts' do 
-   name = params[:name]
-   return "Hello #{name}"
+  configure :development do
+    register Sinatra::Reloader
   end
 
-  post '/submit' do 
-    name = params[:name]
-    message = params[:message]
-    return "Thanks #{name}, you sent this message: #{message}"
-  end 
-
-  get '/names' do 
-    name = params[:name]
-
-    return "#{name}"
-  end
-  
-  get '/hello?' do 
-    name = params[:name]
-    return "Hello #{name}!"
-  end 
-
-  post '/sort-names' do 
-    names = params[:names]
-    sorted = names.split(',').sort!.join(',')
-    
-    return "#{sorted}"
-  end 
+  get'/tasks/new' do 
+    return erb(:new_task)
 end 
+  post'/tasks' do
+    @task_name = params[:task_name]
+    return erb(:task_created)
+  end
+end
+#  get '/' do 
+#    return 'Hello'
+#   end
+
+#   get '/else' do 
+#     # @greet = params[:greet]
+#     return erb(:index)
+#   end 
+
+#   get '/posts' do 
+#    name = params[:name]
+#    return "Hello #{name}"
+#   end
+
+#   post '/submit' do 
+#     name = params[:name]
+#     message = params[:message]
+#     return "Thanks #{name}, you sent this message: #{message}"
+#   end 
+
+#   get '/names' do 
+#     name = params[:name]
+
+#     return "#{name}"
+#   end
+  
+#   get '/hello?' do 
+#     name = params[:name]
+#     return "Hello #{name}!"
+#   end 
+
+#   post '/sort-names' do 
+#     names = params[:names]
+#     sorted = names.split(',').sort!.join(',')
+    
+#     return "#{sorted}"
+#   end 
+# end 
 #routes 
 
 # GET / -> execute some code
--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -7,21 +7,55 @@ describe Application do
 
   let(:app) { Application.new }
 
-  context "GET /names" do
-    it 'returns 200 OK' do
-      # Assuming the post with id 1 exists.
-      response = get('/names', name: 'Julia, Mary, Karim')
+context 'GET /tasks/new' do  
+  it 'should return the html form to create a new task' do 
+    response = get('/tasks/new')
 
-      expect(response.status).to eq(200)
-       expect(response.body).to eq('Julia, Mary, Karim')
-    end
+    expect(response.status).to eq(200)
+    expect(response.body).to include('<form method="POST" action="/tasks">')
+    expect(response.body).to include('<input type="text"name="task_name" />')
+  end 
 
-  #   it 'returns 404 Not Found' do
-  #     response = get('/posts?id=276278')
 
-  #     expect(response.status).to eq(404)
-  #     # expect(response.body).to eq(expected_response)
+context 'POST /tasks' do 
+  it "should create task and return confirmation" do 
+    response = post('/tasks', task_name: 'Buy milk')
+
+    expect(response.status).to eq(200)
+    expect(response.body).to include('<h1>You saved task: Buy milk</h1>')
+  end   
+
+  it 'it should create task and return confirmation page with a differnt name' do
+  response = post('/tasks', task_name: 'Buy shoes')
+
+  expect(response.status).to eq(200)
+  expect(response.body).to include('<h1>You saved task: Buy shoes</h1>')
+  end   
+end 
+
+  # context "GET /names" do
+  #   it 'returns 200 OK' do
+  #     # Assuming the post with id 1 exists.
+  #     response = get('/names', name: 'Julia, Mary, Karim')
+
+  #     expect(response.status).to eq(200)
+  #      expect(response.body).to eq('Julia, Mary, Karim')
   #   end
+
+  #   it 'returns hello in html'do 
+  #     response = get('/else')
+  #     expect(response.body).to include('<h1>Hello!</h1>')
+  #   end 
+
+
+
+  # #   it 'returns 404 Not Found' do
+  # #     response = get('/posts?id=276278')
+
+  # #     expect(response.status).to eq(404)
+  # #     # expect(response.body).to eq(expected_response)
+  # #   end
+  # # end
   # end
-  end
+end 
 end 
